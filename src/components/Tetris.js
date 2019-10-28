ca5469efd59905d0122386e6e8145bfdd8c59301
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-10-28 16:51:35 -0700

    hmmm, still has bugs cannot stop at bottom of game area

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -5,7 +5,7 @@ import { useStage } from '../hooks/useStage';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
-import { createStage } from '../helpers/gameHelpers';
+import { createStage, checkCollision } from '../helpers/gameHelpers';
 
 function Tetris() {
   const [dropTime, setDropTime] = useState(null);
@@ -17,18 +17,30 @@ function Tetris() {
   console.log('re-render');
 
   const movePlayer = dir => {
-    updatePlayerPos({ x: dir, y: 0 });
-  }
+    if(!checkCollision(player, stage, { x: dir, y: 0 })){
+      updatePlayerPos({ x: dir, y: 0 });
+    }
+  };
 
   const startGame = () => {
     //reset everything
     setStage(createStage());
     resetPlayer();
+    setGameOver(false);
   }
 
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false })
-  }
+    if(!checkCollision(player, stage, { x: 0, y: 1 })){
+      updatePlayerPos({ x: 0, y: 1, collided: false })
+    } else {
+      if(player.pos.y < 1) {
+        console.log('GAME OVER!');
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0,  y: 0, collided: true});
+    }
+  };
 
   const dropPlayer = () => {
     drop();
--- src/helpers/gameHelpers.js
+++ src/helpers/gameHelpers.js
@@ -3,6 +3,26 @@ export const STAGE_HEIGHT = 20;
 
 export const createStage = () => 
   Array.from(Array(STAGE_HEIGHT), () =>
-    new Array(STAGE_WIDTH).fill([0, 'clear']),
+    new Array(STAGE_WIDTH).fill([0, 'clear'])
   );
 
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+  for(let y = 0; y < player.tetromino.length; y += 1) {
+    for(let x = 0; x < player.tetromino[y].length; x += 1) {
+      // check to see if we're on a tetromino cell
+      if(player.tetromino[y][x] !== 0) {
+        if(
+          // check that our move is in the  game areas height (y)
+          // cannot go through the bottom of the play area
+          !stage[y + player.pos.y + moveY] || 
+          // check that our move is inside the game areas width (x)
+          !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+          // check that the cell we're moving isn't setto clear
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+};
